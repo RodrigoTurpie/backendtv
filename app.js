81cd73312494cb4d291818bb39d797e72888a4ac
Rodrigo Turpie <jsepulveda_turpie@hotmail.com>
2024-10-31 22:47:47 -0300

    Se crea el server

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-require('dotenv').config
+require('dotenv').config();
 
 const app = express();
 
@@ -7,6 +7,9 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 5001;
 
+app.use('/', (req, res)=>{
+    res.send('Hello World!');
+})
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`);
